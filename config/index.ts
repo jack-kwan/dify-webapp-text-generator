a360be64fec7f6a9df619a99cd3a08dadc039ad1
jack-kwan <34941961+jack-kwan@users.noreply.github.com>
2024-02-24 08:59:19 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -3,9 +3,9 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Text Generator APP',
-  description: 'App description',
-  copyright: '',
+  title: 'AI文案',
+  description: '输入一个词，生成一段话',
+  copyright: '© 唐道科技 2024',
   privacy_policy: '',
   default_language: 'zh-Hans',
 }
